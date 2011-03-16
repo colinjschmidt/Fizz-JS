170041b189b34d8ab3debe8b10fed62fb96bf0d9
Colin Schmidt <cjschmidt23@gmail.com>
2011-03-15 23:01:14 -0700

    Added:     - Double buffering     - zIndex property on entities     - onCollide callback function for each entity     - Lots of comments
    Modified:
        - collision detection loop, removed collisions array and loop.  Using callback on each entity instead.

--- fizz.js
+++ fizz.js
@@ -1,19 +1,21 @@
+/**
+ * Fizz JS - A Simple Javascript Physics Engine
+ * 
+ * @author Colin Schmidt
+ * @version 1.0 3/15/2011
+ */
 var FIZZ = (function(){
   
     var _self = {};
-    
-    
-    var collisionTypes = {
-        'light' : 0,
-        'normal': 1,
-        'heavy' : 2,
-        'static': 3
-    };
     
     _self.World = function (config) {
         
         // Use the first canvas element by default
         this.canvas = document.getElementsByTagName('canvas')[0];
+        
+        // Setup canvas element to use as a buffer
+        this.buffer = document.createElement('canvas');
+        this.bufferContext = this.buffer.getContext('2d');
         
         // The default framerate (ideally)
         this.fps = 100;
@@ -21,102 +23,148 @@ var FIZZ = (function(){
         // The default gravity in pixels/second/second
         this.gravity = -100; 
         
-        // Keeps track of entities and collisions
+        // Keeps track of entities
         this.entities = [];
-        this.collisions = [];
         
         // Keeps track of total steps and time between steps in seconds
-        this.steps = {};
-        this.steps.count = 0;
-        this.steps.delta = 0;
-        this.steps.last = 0;
-        this.steps.record = function () {
-            var t = (new Date()).getTime();
-            if (this.count > 0) {
-                this.delta = (t - this.last) / 1000; // delta in seconds
+        this.steps = {
+            count : 0,
+            delta : 0,
+            last : 0,
+            record : function () {
+                var t = (new Date()).getTime();
+                if (this.count > 0) {
+                    this.delta = (t - this.last) / 1000; // delta in seconds
+                }
+                this.last = t;
+                this.count++;
             }
-            this.last = t;
-            this.count++;
         };
         
+        /**
+         * Main Game Loop, called once per frame
+         * 
+         * Current Loop:
+         *     1. Clear buffer and canvas
+         *     2. Move and check collisions
+         *     3. Render each entity to buffer
+         *     4. Draw buffer to canvas
+         */
         this.step = function () {
                 
-            var world = this;
             this.clear();
             this.draw();
             this.steps.record();
+            
+            // Use setTimeout, rather than set interval so that steps won't overlap on slow connections
+            // Best case scenario, step is called at "framerate" times per second.  
+            // All entity movements are based off of pixels per second and not pixels per frame,
+            // So that entities should move at same speed independent of connection speed
+            var world = this;
             setTimeout(function () {
                 world.step();
             }, 1000/this.fps);
 
         };
         
+        /**
+         * Cleans the buffer and canvas, used between frames
+         */
         this.clear = function () {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.bufferContext.clearRect(0, 0, this.buffer.width, this.buffer.height);
         };
         
+        /**
+         * Draws each entity into the world
+         */
         this.draw = function () {
+            
+            // Move all of the entities
             for (var i = 0, len = this.entities.length; i < len; i++) {
-
-                    var entity = this.entities[i];
+                
+                var entity = this.entities[i];
+                
+                // Update the position of the Entity
+                if (typeof entity.move !== 'undefined') {
                     entity.move(this);
-                    entity.render(this);
-                    for (var j = 0; j < i; j++) {
-                        entity.checkCollision(this.entities[j], this);
-                    }
+                }
+                
+                // Check for collisions on all previously moved items
+                for (var j = 0; j < i; j++) { 
+                    var foreign_entity = this.entities[j];
+                    entity.checkCollision(foreign_entity, this);
+                }
+                
+                entity.render(this);
             }
             
-            this.handleCollisions();
+            // Redraw canvas from buffer
+            this.context.drawImage(this.buffer, 0, 0);
         };
-        this.handleCollisions = function () {
-            for (var i in this.collisions) {
-                if (this.collisions.hasOwnProperty(i)) {
-                    var entity = this.collisions[i].entity;
-                    var plane = this.collisions[i].plane;
-                    var adjustment = this.collisions[i].adjustment;
-                    entity.position[plane] += adjustment;
-                    entity.velocity[plane] = entity.velocity[plane] * -1 * entity.bounciness;
-                } 
-            }
-            this.collisions = [];
+        
+        /**
+         * Adds an entity to the world and reorders entities by zIndex property
+         * higher zIndexes are rendered after (aka on top of) lower zIndexes.
+         */
+        this.addEntity = function (entity) {
+            this.entities.push(entity);
+            this.entities.sort(function(a, b){return a.zIndex - b.zIndex;});
         };
+        
+        /**
+         * Gets the actual Y coordinate on the canvas
+         * 
+         * This function allows us to pretend like the canvas's origin is in the bottom left,
+         * rather than the top left.  This is to make things a bit simpler ... 
+         * positive velocities move up, rather than down for example.
+         */
         this.getRealY = function (y) {
             return this.canvas.height - y;
         };
         
+        // Add all methods and properties in config to this object
         for (var i in config) {
             if (config.hasOwnProperty(i)) { 
                 this[i] = config[i];
             }
         }
         
-        this.context = this.canvas.getContext('2d'); 
-
+        // Add a shortcut to the canvas's context object
+        this.context = this.canvas.getContext('2d');
+        
+        // Setup the buffer attributes
+        this.buffer.height = this.canvas.height;
+        this.buffer.width = this.canvas.width;
     };
     
     _self.Entity = function (config) {
         
+        var collisionTypes = {
+            'light' : 0,
+            'normal': 1,
+            'heavy' : 2,
+            'static': 3
+        };
+        
         this.color = 'black';
         
-        this.position = { x : 0, y : 0};
-        this.velocity = { x : 1, y : 0};
-        this.acceleration = { x : 0, y : 0};
+        this.position = {x : 0, y : 0};
+        this.velocity = {x : 0, y : 0};
+        this.acceleration = {x : 0, y : 0};
+        this.edges = {};
         
         this.gravity = 1;
         this.bounciness = 0;
         this.collisionType = 'normal';
         
-        this.edges = {};
-        this.edges.top = 0;
-        this.edges.right = 0;
-        this.edges.bottom = 0;
-        this.edges.left = 0;
+        this.zIndex = 1;
         
         this.move = function (world) {
 
             this.velocity.x += this.acceleration.x * world.steps.delta;
 
-            if (this.edges.bottom <= 0 && this.bounciness <= 0) {
+            if (this.edges.bottom <= 0 && this.bounciness <= 0 && world.steps.count < 1) {
                 this.acceleration.y = 0;
                 this.velocity.y = 0;
             }
@@ -136,8 +184,33 @@ var FIZZ = (function(){
         };
         
         this.renderTo = function (world) {
+            
             if (typeof world !== 'undefined') {
-                world.entities.push(this);
+                world.addEntity(this);
+                
+                var positionMap = {
+                
+                    x : {
+                        'center' : (world.canvas.width / 2) - (0.5 * this.width),
+                        'right' : (world.canvas.width) - this.width,
+                        'left' : 0
+                    },
+
+                    y : {
+                        'center' : (world.canvas.height / 2) - (0.5 * this.height),
+                        'top' : world.canvas.height,
+                        'bottom' : this.height
+                    }
+                };
+                
+                if (typeof this.position.x == 'string') {
+                    this.position.x = (positionMap.x[this.position.x]) ? positionMap.x[this.position.x] : 0;
+                }
+                
+                if (typeof this.position.y == 'string') {
+                    this.position.y = (positionMap.y[this.position.y]) ? positionMap.y[this.position.y] : this.height;
+                }
+                
                 this.render(world);
             }
             return this;
@@ -148,8 +221,7 @@ var FIZZ = (function(){
             var edges = foreign_entity.edges;
             var opposingX = (foreign_entity.velocity.x < 0 && this.velocity.x < 0 ||foreign_entity.velocity.x > 0 && this.velocity.x > 0);
             var opposingY = (foreign_entity.velocity.y < 0 && this.velocity.y < 0 ||foreign_entity.velocity.y > 0 && this.velocity.y > 0);
-            var distanceX, distanceY, adjustments = {};
-            
+            var plane, distanceX, distanceY, adjustments = {};
             
             /*
              * TODO: Figure out this collision stuff a little more.  
@@ -159,6 +231,7 @@ var FIZZ = (function(){
             
             // check x values
             
+
             // This Entity contained entirely within Foreign Entity's width
             if (this.edges.left >= edges.left && this.edges.right <= edges.right) {
                 //alert('x5');
@@ -252,22 +325,26 @@ var FIZZ = (function(){
                 adjustments.thisEntity = adjustments.total / 2 ;
                 adjustments.foreignEntity = adjustments.total / 2 * -1;
             }
-
-            world.collisions.push({
-                plane : (distanceX < distanceY) ? 'x' : 'y',
-                adjustment : adjustments.thisEntity,
-                opposing : (distanceX > distanceY) ? opposingX : opposingY,
-                entity : this
-            });
             
-            world.collisions.push({
-                plane : (distanceX < distanceY) ? 'x' : 'y',
-                adjustment : adjustments.foreignEntity,
-                opposing : (distanceX > distanceY) ? opposingX : opposingY,
-                entity : foreign_entity
-            });
+            // Figure out which side collided first
+            plane = (distanceX < distanceY) ? 'x' : 'y';
+            this.onCollide(plane, adjustments.thisEntity);
+            foreign_entity.onCollide(plane, adjustments.foreignEntity);
 
         };
+        
+        /**
+         * Resolves any entity overlap (occurs when enities collide between frames)
+         * and reverses (and multiplies) the entities velocity depending on its bounciness
+         */
+        this.onCollide = function (plane, adjustment) {
+            
+            // Move the overlapping entity
+            this.position[plane] += adjustment;
+                    
+            // Reverse the direction of the colliding entity and increase its velocity by its bounciness
+            this.velocity[plane] = this.velocity[plane] * -1 * this.bounciness;
+        }
         
         for (var i in config) {
             if (config.hasOwnProperty(i)) { 
@@ -287,9 +364,9 @@ var FIZZ = (function(){
         this.bounciness = 0;
         this.gravity = 1;
         
-        this.position = { x : 0, y: 0 };
-        this.velocity = { x : 0, y: 0 };
-        this.acceleration = { x : 0, y: 0 };
+        this.position = {x : 0, y: 0};
+        this.velocity = {x : 0, y: 0};
+        this.acceleration = {x : 0, y: 0};
         
         this.computeEdges = function () {
             this.edges.top = this.position.y;
@@ -300,8 +377,8 @@ var FIZZ = (function(){
         
         this.render = function (world) {
             if (typeof world !== 'undefined') {
-                world.context.fillStyle = this.color;
-                world.context.fillRect(this.position.x, world.getRealY(this.position.y), this.width, this.height);
+                world.bufferContext.fillStyle = this.color;
+                world.bufferContext.fillRect(this.position.x, world.getRealY(this.position.y), this.width, this.height);
             }
         };
         
@@ -325,9 +402,9 @@ var FIZZ = (function(){
         this.bounciness = 0;
         this.gravity = 1;
         
-        this.position = { x : 0, y: 0 };
-        this.velocity = { x : 0, y: 0 };
-        this.acceleration = { x : 0, y: 0 };
+        this.position = {x : 0, y: 0};
+        this.velocity = {x : 0, y: 0};
+        this.acceleration = {x : 0, y: 0};
         
         this.computeEdges = function () {
             this.edges.top = this.position.y + this.radius;
@@ -338,11 +415,11 @@ var FIZZ = (function(){
         
         this.render = function (world) {
             if (typeof world !== 'undefined') {
-                world.context.beginPath();
-                world.context.fillStyle = this.color;
-                world.context.arc(this.position.x, world.getRealY(this.position.y), this.radius, 0, Math.PI * 2,  true);
-                world.context.closePath();
-                world.context.fill();
+                world.bufferContext.beginPath();
+                world.bufferContext.fillStyle = this.color;
+                world.bufferContext.arc(this.position.x, world.getRealY(this.position.y), this.radius, 0, Math.PI * 2,  true);
+                world.bufferContext.closePath();
+                world.bufferContext.fill();
             }
         };
         
